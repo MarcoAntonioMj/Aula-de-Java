bdb30bd3af019e64a86909169693e01523ad353b
Marco Antonio <marcotonho38@gmail.com>
2023-03-30 18:57:57 -0300

    Alterando exr 8

--- Lista de exercicios 1 sequencial/Aulalista8.java
+++ Lista de exercicios 1 sequencial/Aulalista8.java
@@ -1,21 +1,21 @@
 import java.util.Scanner;
-public class Main {
-    public static void Aulalista8 (String[]args)
-  {
-    Scanner sc = new Scanner (System.in);
-    //ENTRADA
-      System.out.println ("**Algoritmo de calculo**");
-      System.out.print ("Quanto o total de veiculos ? ");
-      int totalVeiculos = sc.nextInt ();
-      System.out.print ("Quanto o total de rodas ? ");
-      int totalRodas = sc.nextInt ();
-    //PROCESSAMENTO
-    int paresRoda = totalRodas/2;
+
+public class Aulalista8 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    // ENTRADA
+    System.out.println("**Algoritmo de calculo**");
+    System.out.print("Quanto o total de veiculos ? ");
+    int totalVeiculos = sc.nextInt();
+    System.out.print("Quanto o total de rodas ? ");
+    int totalRodas = sc.nextInt();
+    // PROCESSAMENTO
+    int paresRoda = totalRodas / 2;
     int motos = paresRoda - totalVeiculos;
     totalRodas = totalRodas - motos * 2;
-    int carros = totalRodas/4;
-    //SAIDA
+    int carros = totalRodas / 4;
+    // SAIDA
     System.out.println("Há " + carros + " carros e " + motos + " motos no estacionamento.");
     sc.close();
-	}
-	}
+  }
+}
